4b09c6ad1bf85c6938703bc34b50a6f7e82a9bc5
“Ivy <ivys725@gmail.com>
2018-06-14 18:40:44 +0200

    Post and get favouritelist

---
+++ dist10/src/controllers/user-info.controller.js
@@ -0,0 +1,47 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const repository_1 = require("@loopback/repository");
+const users_repository_1 = require("../repositories/users.repository");
+const rest_1 = require("@loopback/rest");
+const jsonwebtoken_1 = require("jsonwebtoken");
+let UserInfoController = class UserInfoController {
+    constructor(usersRepo) {
+        this.usersRepo = usersRepo;
+    }
+    async getUser(jwt) {
+        if (!jwt)
+            throw new rest_1.HttpErrors.Unauthorized('JWT token is required.');
+        try {
+            var jwtBody = jsonwebtoken_1.verify(jwt, 'encryption');
+            return jwtBody;
+        }
+        catch (err) {
+            throw new rest_1.HttpErrors.BadRequest('JWT token invalid');
+        }
+    }
+};
+__decorate([
+    rest_1.get('/User'),
+    __param(0, rest_1.param.query.string('jwt')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], UserInfoController.prototype, "getUser", null);
+UserInfoController = __decorate([
+    __param(0, repository_1.repository(users_repository_1.UsersRepository.name)),
+    __metadata("design:paramtypes", [users_repository_1.UsersRepository])
+], UserInfoController);
+exports.UserInfoController = UserInfoController;
+//# sourceMappingURL=user-info.controller.js.map
